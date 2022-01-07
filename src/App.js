a9fb5b55462429fc7b3153caaecf3cfe26ba1bcc
Pete Byrne <petebyrne77@gmail.com>
2022-01-07 16:30:22 +0000

    Second Commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function App(props) {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello Codecademy!
         </p>
+        <p>{props.name}</p>
         <a
           className="App-link"
           href="https://reactjs.org"
